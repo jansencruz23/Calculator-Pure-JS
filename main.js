c93ba158e6dee5d53787fc6333bcc9bd4cf6d524
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2023-02-19 19:08:54 +0800

    Add keylisteners

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ let lastOp;
 let currOp;
 let prev;
 
+let btnsNum = [];
+let btnsOp = [];
+
 btnNum.forEach(btn => {
+    btnsNum += btn.textContent;
     btn.addEventListener('click', (e) => {
         if(curr.textContent.length <= 16)
             btnNumClicked(e);
@@ -17,11 +21,40 @@ btnNum.forEach(btn => {
 });
 
 btnOp.forEach(btn => {
+    btnsOp += btn.textContent;
     btn.addEventListener('click', (e) => {
         audio.play();
         btnOpClicked(e);
     });
 });
+
+document.addEventListener('keyup', (e) => {
+    if(curr.textContent.length <= 16){
+        if(btnsNum.includes(e.key)){
+            btnNumClicked2(e);
+        }
+        else if(btnsOp.includes(e.key)){
+            btnOpClicked2(e);
+        }
+        else if(e.key == 'Backspace'){
+            curr.textContent = '0';
+            reset();
+        }
+    }
+})
+
+function btnNumClicked2(e) {
+    audio.play();
+    if(e.key == '.')
+    {
+        if(!curr.textContent.includes('.'))
+            curr.textContent += '.';
+    }
+    else if(curr.textContent == '0')
+        curr.textContent = e.key;
+    else   
+        curr.textContent += e.key;
+}
 
 function btnNumClicked(e) {
     audio.play();
@@ -38,6 +71,46 @@ function btnNumClicked(e) {
 
 function btnOpClicked(e) {
     switch(e.target.textContent)
+    {
+        case 'AC':
+            curr.textContent = '0';
+            prevNum.textContent = '';
+            prevOp.style.visibility = "hidden";
+            currOp = '';
+            break;
+        case 'C':
+            curr.textContent = (curr.textContent.length == 1) 
+                ? curr.textContent = '0' 
+                : curr.textContent.substring(0, curr.textContent.length -1);
+            break;
+        case '-/+':
+            let num = Number(curr.textContent);
+            curr.textContent = num * -1;
+            break;
+        case '/':
+            currOp = '/';
+            signsClicked(currOp);
+            break;
+        case 'x':
+            currOp = '*';
+            signsClicked(currOp);
+            break;
+        case '-':
+            currOp = '-';
+            signsClicked(currOp);
+            break;
+        case '+':
+            currOp = '+';
+            signsClicked(currOp);
+            break;
+        case '=':
+            curr.textContent = operate();
+            break;
+    }
+}
+
+function btnOpClicked2(e) {
+    switch(e.key)
     {
         case 'AC':
             curr.textContent = '0';
